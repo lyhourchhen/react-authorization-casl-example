8de1badc1a8977a57bc3e839e459255486db8cdd
LyhourChhen <ukamilove66@gmail.com>
2021-05-13 19:38:17 +0700

    update role

--- src/casl/ability.ts
+++ src/casl/ability.ts
@@ -3,20 +3,17 @@ import { AbilityBuilder, Ability, AbilityClass } from "@casl/ability";
 type Actions = "manage" | "create" | "read" | "update" | "delete";
 type Subjects = "Todo" | "Config";
 
+// * If from server
+
 export type AppAbility = Ability<[Actions, Subjects]>;
 export const AppAbility = Ability as AbilityClass<AppAbility>;
 
 export default function defineRulesFor(role: string) {
   const { can, rules } = new AbilityBuilder(AppAbility);
 
-  // if (role === "role") {
-  //   can("manage", "all");
-  // } else {
-  //   can(["read", "create"], "Todo");
-  // }
   switch (role) {
     case "super-admin":
-      can("manage", ["Todo", "Config"]);
+      can("manage", ["Config", "Todo"]);
     case "admin":
       can(["create", "read", "delete"], "Todo");
     case "staff":
